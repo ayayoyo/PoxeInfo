cf632d785bac9aef93e67bafcc30d2726e456058
Aya <aya_elsahhar@yahoo.com>
2016-02-17 11:49:32 -0500

    Images, Music, CSV are added

---
+++ PoxeInfo/Pokemon.swift
@@ -0,0 +1,35 @@
+//
+//  Pokemon.swift
+//  PoxeInfo
+//
+//  Created by Aya on 2/17/16.
+//  Copyright © 2016 Aya. All rights reserved.
+//
+
+import Foundation
+
+private var _Name: String!
+private var _PokeId: String!
+
+var Name: String
+{
+    get
+    {
+        return _Name
+    }
+}
+
+var PokeId: String
+{
+    get
+    {
+        return _PokeId
+    }
+}
+
+/*init(name: String , pokid: String)
+{
+    _Name = name
+    _PokeId = pokid
+}*/
+ 
